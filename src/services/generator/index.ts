a09ae5bb0f4c09ccf27aea71c049e7ffbd44ad07
Allan Ma <remoolan@gmail.com>
2026-04-08 13:24:23 +0800

    feat(generator): add service types and barrel exports (#26)

---
+++ src/services/generator/index.ts
@@ -0,0 +1,10 @@
+export { GooglePlacesClient } from "./google-places.client";
+export { slugify, generateUniqueSlug } from "./slug.util";
+export type {
+  CreateGenerationInput,
+  CreateGenerationResult,
+  GenerationStatusResult,
+  GenerationStatus,
+  PlaceDetails,
+} from "./generator.types";
+export { GENERATION_STATUS } from "./generator.types";
