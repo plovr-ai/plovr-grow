f4c12a74414490aed887cf7e3906805ed60c8cba
Allan Ma <remoolan@gmail.com>
2026-04-11 15:04:56 +0800

    chore: remove hand-written migration, will be regenerated via migrate dev

--- prisma/migrations/20260411055042_square_menu_compat/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE `tax_configs` ADD COLUMN `inclusion_type` VARCHAR(191) NOT NULL DEFAULT 'additive';
-
--- AlterTable
-ALTER TABLE `external_id_mappings` ADD COLUMN `external_version` BIGINT NULL;
-
--- AlterTable
-ALTER TABLE `integration_sync_records` ADD COLUMN `stats` JSON NULL;
